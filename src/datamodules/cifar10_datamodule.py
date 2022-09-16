aeab4a1e1e8faa49f0f1d99e67af0bd15c9c0e3e
garima-mahato <m.garima318@gmail.com>
2022-09-17 01:31:39 +0530

    edited files for timm and cifar10 specific changes

--- src/datamodules/cifar10_datamodule.py
+++ src/datamodules/cifar10_datamodule.py
@@ -51,7 +51,7 @@ class CIFAR10DataModule(LightningDataModule):
 
         # data transformations
         self.transforms = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])]
+            [transforms.Resize(224),transforms.ToTensor(), transforms.Normalize(mean=[0.485,0.456,0.406],std=[0.229,0.224,0.225])]
         )
 
         self.data_train: Optional[Dataset] = None
